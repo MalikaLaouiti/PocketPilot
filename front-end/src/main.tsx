5e49c3d1750567cf79ab10be75b48d17fcc3d6de
MalikaLaouiti <laouiti.malika@yahoo.com>
2026-06-30 16:51:31 +0100

    feat(frontend): add React Query provider for server state management

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import theme from './theme'
 import './styles/fonts.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
